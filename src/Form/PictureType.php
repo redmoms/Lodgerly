3677d23a415d671907e2df64c1931c12315ecb54
Mohamed <moha@mail.com>
2025-01-06 10:25:03 +0100

    .

--- src/Form/PictureType.php
+++ src/Form/PictureType.php
@@ -4,16 +4,31 @@ namespace App\Form;
 
 use App\Entity\Picture;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class PictureType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('link')
+            ->add('name',  FileType::class, [
+                'label' => 'Picture',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'veuillez insérer le bon format ',
+                    ])
+                ],
+            ])
         ;
     }
 
