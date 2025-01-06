8582d9c8bd051de9efaa528c84c151a93f0b60fb
Mohamed <moha@mail.com>
2025-01-06 12:25:40 +0100

    implementation d'un système d'upload

--- src/Controller/PictureController.php
+++ src/Controller/PictureController.php
@@ -5,11 +5,13 @@ namespace App\Controller;
 use App\Entity\Picture;
 use App\Form\PictureType;
 use App\Repository\PictureRepository;
+use App\Service\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/picture')]
 final class PictureController extends AbstractController
@@ -23,17 +25,29 @@ final class PictureController extends AbstractController
     }
 
     #[Route('/new', name: 'app_picture_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, FileUploader $fileUploader): Response
     {
         $picture = new Picture();
         $form = $this->createForm(PictureType::class, $picture);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($picture);
-            $entityManager->flush();
+            $pictureFile = $form->get('name')->getData();
+            
+            if ($pictureFile) {
+                $name = $fileUploader->upload($pictureFile);
+            }
+            
+            // ... persist the $product variable or any other work
+            $picture->setName($name);
+            $picture->setLink($this->getParameter('kernel.project_dir').'/public/images/'.$name);
 
-            return $this->redirectToRoute('app_picture_index', [], Response::HTTP_SEE_OTHER);
+            $entityManager->persist($picture);
+            $entityManager->flush($picture);
+            
+        
+
+            return $this->redirectToRoute('app_picture_index');
         }
 
         return $this->render('picture/new.html.twig', [
